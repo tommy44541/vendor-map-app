0cfce295a8ae241e538a1664a190aee51d19acd4
tom <wangtao19960223@gmail.com>
2025-08-11 16:49:11 +0800

    feat(all): 專案架設

---
+++ app/vendor/[id].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const VendorDetails = () => {
+  return (
+    <View>
+      <Text>VendorDetails</Text>
+    </View>
+  );
+};
+
+export default VendorDetails;
---
+++ components/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+interface SearchBarProps {
+  onPress: () => void;
+  placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: SearchBarProps) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={() => {}}
+        placeholder="Search"
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#ab85db"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
